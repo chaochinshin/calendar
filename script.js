067a86234a81c08d4d05fd37c63ae56baf52354e
unknown <chaochinshin@gmail.com>
2020-03-22 21:24:41 -0400

    calendar homework

---
+++ script.js
@@ -0,0 +1,76 @@
+
+//On Ready
+$(document).ready(function(){
+
+
+//Save button functionality
+$(".saveBtn").on("click", function(){
+//get nearby values
+var value = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id");
+//save to localStorage variables
+localStorage.setItem(time, value);
+})
+//Hour Updater
+function hourUpdater(){
+    //getrs the current number of hours
+    var currentHour = moment().hours();
+    //var currentdate = getdate().time();
+    //loop
+    $(".time-block").each(function(){
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    //    console.log(blockHour);
+        if(blockHour < currentHour){
+            $(this).addClass("past");
+        }
+        else if(blockHour === currentHour){
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else
+        {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+
+
+//Calling hourUpdater
+hourUpdater();
+
+var internal = setInterval(hourUpdater, 15000);
+//Loading Local Storage saved
+
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#hour-6 .description").val(localStorage.getItem("hour-6"));
+$("#hour-7 .description").val(localStorage.getItem("hour-7"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));
+$("#hour-19 .description").val(localStorage.getItem("hour-19"));
+$("#hour-20 .description").val(localStorage.getItem("hour-20"));
+$("#hour-21 .description").val(localStorage.getItem("hour-21"));
+$("#hour-22 .description").val(localStorage.getItem("hour-22"));
+$("#hour-23 .description").val(localStorage.getItem("hour-23"));
+$("#hour-24 .description").val(localStorage.getItem("hour-24"));
+
+
+//Displaying current day on the page; # is for id and . is for class
+$("#currentDay").text(moment().format("dddd  MMMM Do, YYYY"));
+
+
+});
